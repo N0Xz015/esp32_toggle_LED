fde3774c3cae24a83c5b859f2362538a00d6e9a4
N0Xz015 <darkwishper15@gmail.com>
2025-08-27 03:33:03 +0700

    fix & more comment

--- src/main.c
+++ src/main.c
@@ -1,46 +1,51 @@
-#include <driver/gpio.h>
-#include <esp_system.h>
-#include <stdint.h>
-#include <esp_sntp.h>
-#include <esp_timer.h>
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "esp_timer.h"
+#include "esp_log.h"
+#include "esp_task_wdt.h"
 
-#define LED_PIN_1 12
-#define LED_PIN_2 13
-#define LED_PIN_3 11
+const gpio_num_t LED_PIN_1 = GPIO_NUM_13;
+const gpio_num_t LED_PIN_2 = GPIO_NUM_12;
+const gpio_num_t LED_PIN_3 = GPIO_NUM_11;
+
 #define BLINK_AMOUNT 10 
-#define BLINK_PERIOD 10000
-const size_t LAST_TOGGLE  = 0;
+#define BLINK_PERIOD_MS 2000
 
-void loop1(void *tuggle_led) {
-    size_t LAST_TOGGLE = 0;
-    size_t BLINK_COUNT = 0;
-
-    while (1) { 
-        size_t CURRENT_TIME = esp_timer_get_time();
-        gpio_set_level(LED_PIN_1, 0);
-        gpio_set_level(LED_PIN_2, 0); 
-        if (CURRENT_TIME - LAST_TOGGLE >= BLINK_PERIOD) { // did i set BLINK_PEROD to low?
-            LAST_TOGGLE = CURRENT_TIME;
-            gpio_set_level(LED_PIN_1, !LED_PIN_1); // look like this line didn't work.
-            BLINK_COUNT++; // seem like it work.
-            }
-        else {
-            gpio_set_level(LED_PIN_1, 0);
-        }
-        if(BLINK_COUNT >= BLINK_AMOUNT) {
-            gpio_set_level(LED_PIN_3, !LED_PIN_3); // LED didn't on IDK why.
-            gpio_set_level(LED_PIN_2, 1);// {  
-            gpio_set_level(LED_PIN_1, 1);// this work.
-            vTaskDelay(pdMS_TO_TICKS(3000));// }
-            break;
-        }
-        
-    }
-}
 void app_main() {
+
+    size_t BLINK_COUNT = 0;
+    uint64_t CURRENT_TIME = 0;
+    uint64_t LAST_TOGGLE  = 0;
+    
+    gpio_set_level(LED_PIN_1, 0);
+    gpio_set_level(LED_PIN_2, 0);
+    gpio_set_level(LED_PIN_3, 0);
     gpio_set_direction(LED_PIN_1, GPIO_MODE_OUTPUT);
     gpio_set_direction(LED_PIN_2, GPIO_MODE_OUTPUT);
     gpio_set_direction(LED_PIN_3, GPIO_MODE_OUTPUT);
+    
 
-    xTaskCreate(loop1, "blink_led1", 2048, NULL, 5, NULL);// this line helped by COPILOT.
+    while (1) {  
+        CURRENT_TIME = (uint64_t) (esp_timer_get_time() / 1000);
+        
+        if (CURRENT_TIME - LAST_TOGGLE >= BLINK_PERIOD_MS) { 
+            gpio_set_level(LED_PIN_2, 0);
+            LAST_TOGGLE = CURRENT_TIME;
+            
+            if (LED_PIN_1 == 0) { // when time has come if LED off set to on
+                gpio_set_level(LED_PIN_1, 1);   
+            }
+            else { // when time has come if LED on turn it off
+                gpio_set_level(LED_PIN_1, 0);
+                gpio_set_level(LED_PIN_2, 1);// turn LED on every time that BLINK_COUNT increase
+                BLINK_COUNT++;
+            }
+        }
+        if(BLINK_COUNT >= BLINK_AMOUNT) {
+            gpio_set_level(LED_PIN_3, 1);// this LED related to is progress done // this work well
+            gpio_set_level(LED_PIN_2, 1);// should toggle but it's always on
+            gpio_set_level(LED_PIN_1, 1);// it not blink or blink too fast?
+            break;
+        }
+    }
 }
