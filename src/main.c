b4339fe033270485127f967f4b5e930645639c4e
N0Xz015 <darkwishper15@gmail.com>
2025-08-28 01:59:20 +0700

    modified (still not blink)

--- src/main.c
+++ src/main.c
@@ -4,48 +4,54 @@
 #include "esp_log.h"
 #include "esp_task_wdt.h"
 
-const gpio_num_t LED_PIN_1 = GPIO_NUM_13;
-const gpio_num_t LED_PIN_2 = GPIO_NUM_12;
-const gpio_num_t LED_PIN_3 = GPIO_NUM_11;
+const char* TAG = "LED BLINKER";
 
 #define BLINK_AMOUNT 10 
-#define BLINK_PERIOD_MS 2000
+#define BLINK_PERIOD_MS 500
+
+const gpio_num_t LED_PIN = GPIO_NUM_13;
 
 void app_main() {
 
-    size_t BLINK_COUNT = 0;
+    
     uint64_t CURRENT_TIME = 0;
-    uint64_t LAST_TOGGLE  = 0;
-    
-    gpio_set_level(LED_PIN_1, 0);
-    gpio_set_level(LED_PIN_2, 0);
-    gpio_set_level(LED_PIN_3, 0);
-    gpio_set_direction(LED_PIN_1, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_PIN_2, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_PIN_3, GPIO_MODE_OUTPUT);
-    
+    uint64_t LAST_TOGGLE = 0;
+    size_t BLINK_COUNT = 0;
+    bool LEDSTATE = false;
+   
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
-    while (1) {  
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+    ESP_LOGI(TAG, "starting blinking...");
+
+    while(true) {  
         CURRENT_TIME = (uint64_t) (esp_timer_get_time() / 1000);
-        
+        ESP_LOGV(TAG, "Current time: %" PRIu64 "ms", CURRENT_TIME);
+
         if (CURRENT_TIME - LAST_TOGGLE >= BLINK_PERIOD_MS) { 
-            gpio_set_level(LED_PIN_2, 0);
+            
             LAST_TOGGLE = CURRENT_TIME;
             
-            if (LED_PIN_1 == 0) { // when time has come if LED off set to on
-                gpio_set_level(LED_PIN_1, 1);   
-            }
-            else { // when time has come if LED on turn it off
-                gpio_set_level(LED_PIN_1, 0);
-                gpio_set_level(LED_PIN_2, 1);// turn LED on every time that BLINK_COUNT increase
+            if (LEDSTATE == false) { // when time has come if LED off set to on
+                gpio_set_level(LED_PIN, LEDSTATE);
                 BLINK_COUNT++;
+                ESP_LOGD(TAG, "LED state: %d, counter %u", LEDSTATE, BLINK_COUNT);
             }
+
+            else { // when time has come if LED on turn it off
+                gpio_set_level(LED_PIN, false);
+                ESP_LOGD(TAG, "LED state: %d", LEDSTATE);
+            }
+
         }
+
         if(BLINK_COUNT >= BLINK_AMOUNT) {
-            gpio_set_level(LED_PIN_3, 1);// this LED related to is progress done // this work well
-            gpio_set_level(LED_PIN_2, 1);// should toggle but it's always on
-            gpio_set_level(LED_PIN_1, 1);// it not blink or blink too fast?
+            gpio_set_level(LED_PIN, 1);
             break;
         }
+
+        vTaskDelay(5);
     }
+
+    ESP_LOGI(TAG, "Blinking done. Blinked %u times...", BLINK_COUNT);
 }
