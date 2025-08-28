cc5f87322d31af2a119334b1fe7a8ec16e66d215
Sergio <101521439+EqScript@users.noreply.github.com>
2025-08-28 14:44:31 +0700

    Logic fix hint, code style comments

--- src/main.c
+++ src/main.c
@@ -13,11 +13,11 @@ const gpio_num_t LED_PIN = GPIO_NUM_13;
 
 void app_main() {
 
-    
-    uint64_t CURRENT_TIME = 0;
-    uint64_t LAST_TOGGLE = 0;
-    size_t BLINK_COUNT = 0;
-    bool LEDSTATE = false;
+    /* CONST_ARE_UPCASE but regular variables areCamelCase - why always yell? */
+    uint64_t CURRENT_TIME = 0;       /* currentTime */
+    uint64_t LAST_TOGGLE = 0;        /* lastToggle */   
+    size_t BLINK_COUNT = 0;          /* blinkCounter */
+    bool LEDSTATE = false;           /* well, I guess you got it :) */
    
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
@@ -32,8 +32,8 @@ void app_main() {
             
             LAST_TOGGLE = CURRENT_TIME;
             
-            if (LEDSTATE == false) { // when time has come if LED off set to on
-                gpio_set_level(LED_PIN, LEDSTATE);
+            if (LEDSTATE == false) { // when time has come if LED off set to on /* That means the state is false (there is the value '0' written now) */ 
+                gpio_set_level(LED_PIN, LEDSTATE); /* So, we write that '0' into the LED GPIO, setting it to OFF, means no change */
                 BLINK_COUNT++;
                 ESP_LOGD(TAG, "LED state: %d, counter %u", LEDSTATE, BLINK_COUNT);
             }
